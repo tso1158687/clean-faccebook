527ced66adb18a9c13a75ec84f2f192734ee41c6
jason zheng <tso1158687@gmail.com>
2024-06-26 23:17:39 +0800

    Adjust description

--- content.js
+++ content.js
@@ -2,7 +2,6 @@ const navigationDivs = document.querySelectorAll('div[role="navigation"]');
 const complementary = document.querySelectorAll('div[role="complementary"]');
 const region = document.querySelectorAll('div[role="region"]');
 const banner = document.querySelectorAll('div[role="banner"]');
-const links = document.querySelectorAll('a[href*="ads"]');
 
 navigationDivs.forEach((div) => div.remove());
 complementary.forEach((div) => div.remove());
